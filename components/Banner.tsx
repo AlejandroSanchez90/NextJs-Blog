a19305cec02f51f625ffa67fecd7927a3d95d68c
alejo9010 <alejandrosanchez9010@gmail.com>
2023-04-02 11:31:54 -0500

    Banner Component

---
+++ components/Banner.tsx
@@ -0,0 +1,21 @@
+type Props = {};
+
+function Banner({}: Props) {
+  return (
+    <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 mb-10'>
+      <div>
+        <h1 className='text-7xl'>Ale&apos;s Daily Blog</h1>
+        <h2 className='mt-5 md:mt-1'>
+          Welcome to{' '}
+          <span className='underline decoration-4 decoration-[#20c997]'>Every Developers</span>{' '}
+          favorite blog
+        </h2>
+      </div>
+      <p className='mt-5 md:mt-2 text-gray-400 max-w-sm'>
+        New product features | The last in technology | The weekly debugging
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
