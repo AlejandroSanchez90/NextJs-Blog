7f4bf262b0a00cbd2603d16e85f9fc8e776e37b1
alejo9010 <alejandrosanchez9010@gmail.com>
2023-04-02 14:14:28 -0500

    Types definitions

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
-
+import { previewData } from 'next/headers';
+import { groq } from 'next-sanity';
+import { client } from '@/lib/sanity.client';
+import PreviewSuspense from '../../components/PreviewSuspense';
+import PreviewBlogList from '@/components/PreviewBlogList';
+import BlogList from '@/components/BlogList';
+export const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->,
+  } | order(_createdAt desc)
+`;
 type Props = {};
 
-export default function page({}: Props) {
-  return <div></div>;
+export default async function page({}: Props) {
+  if (previewData() || !previewData())
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role='status'>
+            <p className='text-center text-lg animate-pulse text-[#20c997]'>Loading preview data</p>
+          </div>
+        }>
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
+
+  const posts = await client.fetch(query);
+
+  return <BlogList posts={posts} />;
 }
