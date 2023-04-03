12b67c4b394591daae208d968cfab38996f9a820
alejo9010 <alejandrosanchez9010@gmail.com>
2023-04-03 14:36:43 -0500

    Sanity preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
-
+import { defaultDocumentNode } from './structure';
 export default defineConfig({
   basePath: '/studio',
   name: 'Alejandro_Content_Studio',
@@ -10,7 +10,12 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: defaultDocumentNode,
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
